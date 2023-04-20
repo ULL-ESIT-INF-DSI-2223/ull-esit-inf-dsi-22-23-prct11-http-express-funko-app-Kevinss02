4b2660ae3a77fed14fff40b5d1d3bd091cf738f5
Kevin Salcedo Santana <72339683+Kevinss02@users.noreply.github.com>
2023-04-20 10:20:14 +0000

    Delete dist directory

--- dist/Ejercicio/commands.d.ts
+++
@@ -1,2 +0,0 @@
-import { ResponseType } from './types.js';
-export declare const readCommand: (title: string, cb: (err: string | undefined, res: ResponseType | undefined) => void) => void;
--- dist/Ejercicio/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type Command = {
-    title: string;
-    output: string;
-};
-export type ResponseType = {
-    type: 'add' | 'remove' | 'read' | 'list';
-    success: boolean;
-    command: Command | undefined;
-};
